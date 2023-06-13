24881ea6dcc54299cd6ee4f46043ac854e9aa875
Aubrian Duhayon <aubrian.duhayon@mail-ecv.fr>
2023-06-13 10:22:35 +0200

    :sparkles: Ajout sentry-expo

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://12db23e3737640f39f3457fa196f928f@o4505351183073280.ingest.sentry.io/4505351190872064',
+  enableInExpoDevelopment: true,
+  debug: true,
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto"/>
+      </View>
   );
 }
 
