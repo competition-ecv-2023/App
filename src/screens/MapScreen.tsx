ea12960c3975a47d20f45c1152c31ddfa74d8b21
Aubrian Duhayon <aubrian.duhayon@mail-ecv.fr>
2023-06-14 16:26:00 +0200

    :sparkles: Ajout marker avec zone de recherche

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,16 +1,19 @@
 import ScreenContainer from "../components/ScreenContainer";
-import MapView from "react-native-maps";
+import MapView, {Circle, LatLng, Marker} from "react-native-maps";
 import {StyleSheet} from 'react-native';
 import {useEffect, useState} from "react";
 
 import * as Location from 'expo-location';
 import {LocationObject} from "expo-location";
+import {Text} from "@ui-kitten/components";
 
 const MapScreen = () => {
 
     // @ts-ignore
     const [location, setLocation] = useState<LocationObject|undefined>(undefined);
     const [errorMsg, setErrorMsg] = useState<String|undefined>(undefined);
+
+    const [markerCoords, setMarkerCoords] = useState<LatLng>({latitude: 0, longitude: 0});
 
     useEffect(() => {
         (async () => {
@@ -26,13 +29,37 @@ const MapScreen = () => {
         })();
     }, []);
 
+    useEffect(() => {
+        setMarkerCoords({latitude: location?.coords.latitude || 0, longitude: location?.coords.longitude || 0});
+    }, [location]);
+
+    if (!location) {
+        return <Text>Loading ...</Text>
+    }
+
     return (
         <ScreenContainer>
             <MapView
                 style={styles.map}
                 showsUserLocation={true}
                 showsMyLocationButton={true}
-            />
+                initialCamera={{
+                    zoom: 11,
+                    center: {
+                        latitude: location?.coords.latitude || 0,
+                        longitude: location?.coords.longitude || 0,
+                    },
+                    heading: 0,
+                    pitch: 0
+                }}
+            >
+                <Marker
+                    coordinate={{latitude: location?.coords.latitude || 0, longitude: location?.coords.longitude || 0}}
+                    draggable
+                    onDrag={(e) => setMarkerCoords(e.nativeEvent.coordinate)}
+                />
+                <Circle center={markerCoords} radius={10000} strokeColor={'red'} strokeWidth={2}/>
+            </MapView>
         </ScreenContainer>
     )
 }
