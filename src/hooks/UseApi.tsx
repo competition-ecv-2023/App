5244925fc173a14dbcc642cc01d685df6e709c97
Aubrian Duhayon <aubrian.duhayon@mail-ecv.fr>
2023-06-16 11:33:45 +0200

    Update UseApi.tsx

--- src/hooks/UseApi.tsx
+++ src/hooks/UseApi.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const useApi = () => {
     return axios.create({
-        baseURL: "https://09a2-2001-861-34c7-59d0-8e6-1919-bc83-6fb7.ngrok-free.app/patperdue/v1/",
+        baseURL: "https://cc0e-2001-861-34c7-59d0-bcc5-9f11-5f48-9f2e.ngrok-free.app/patperdue/v1/",
         headers: {
             "Authorization": "Bearer ZKqZfeQZtJpXWuBe3T4HBr5FSWFdc6zhQPmeZDrEtQBUnxSxgDqNRSAw3CzSEyu8FPt6aNgHrVj4j7tfyGpLwGzm3sgz8qLdCTykHYEaGJm3ykGvPG5qznaK",
             "Content-Type": "application/x-www-form-urlencoded"
