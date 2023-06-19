8ed13e0ec3a4b4c10cd09b0ae3c5314d23ff722c
CoralyM <coraly.mgk@gmail.com>
2023-06-19 15:06:00 +0200

    Adding custom animal name on advert confirmation screen.

--- src/screens/AdvertConfirmation.tsx
+++ src/screens/AdvertConfirmation.tsx
@@ -2,18 +2,22 @@ import {Text, Layout} from "@ui-kitten/components";
 import ScreenContainer from "../components/ScreenContainer";
 import {StyleSheet} from "react-native";
 import OutlineButton from "../components/OutlineButton";
+import { useContext } from "react";
+import CreateAdvertContext from "../context/CreateAdvertContext";
 
 const AdvertConfirmation = () => {
+
+    const {advert} = useContext(CreateAdvertContext);
 
     return (
 
             <ScreenContainer withScroll backgroundColor={'#fff'}>
                 <Layout style={{padding: 20, backgroundColor: "#fff"}}>
                     <Text style={styles.title}>Votre annonce est publiée !</Text>
-                    <Text style={styles.text}>L'annonce de a été publiée sur Pat'Perdue ainsi que sur le groupe Facebook de votre région.</Text>
+                    <Text style={styles.text}>L'annonce de {advert.animalName} a été publiée sur Pat'Perdue ainsi que sur le groupe Facebook de votre région.</Text>
                     <OutlineButton title="Accéder à l'annonce"/>
                     <Text style={styles.title}>Contactez la communauté</Text>
-                    <Text style={styles.text}>Les utilisateurs se trouvant dans la zone de recherche vont être alerter de la fugue de !</Text>
+                    <Text style={styles.text}>Les utilisateurs se trouvant dans la zone de recherche vont être alerter de la fugue de {advert.animalName} !</Text>
                     <Text style={styles.text}>Ils pourront déclarer l'animal comme retrouvé et vous contacter en cas d'informations.</Text>
                     <OutlineButton title="Voir la carte"/>
                 </Layout>
