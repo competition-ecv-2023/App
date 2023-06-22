481b52f7db793c335a414fe140d0ef95b09984e0
Aubrian Duhayon <aubrian.duhayon@mail-ecv.fr>
2023-06-22 17:57:59 +0200

    Update AdvertScreen.tsx

--- src/screens/AdvertScreen.tsx
+++ src/screens/AdvertScreen.tsx
@@ -5,16 +5,20 @@ import React, {useEffect, useState} from "react";
 import {FlatList, Modal, StatusBar, StyleSheet, TouchableOpacity, View} from "react-native";
 import {BackIcon} from "../components/CustomHeader";
 import {Image} from "expo-image";
-import {API_ADDRESS} from "../hooks/UseApi";
+import {API_ADDRESS, useApi} from "../hooks/UseApi";
 import dayjs from "dayjs";
 import OutlineButton from "../components/OutlineButton";
 import MapView, {Circle, Marker} from "react-native-maps";
 import {useUserLocation} from "../context/UserLocation";
 import ImageViewer from "react-native-image-zoom-viewer";
+import {Routes} from "../navigation/Route";
+import {showMessage} from "react-native-flash-message";
+import {AxiosError} from "axios";
 
 const AdvertScreen = ({navigation, route}: BottomTabScreenProps<any>) => {
 
-    const { location } = useUserLocation();
+    const {location} = useUserLocation();
+    const api = useApi();
     const [animal, setAnimal] = useState<any | undefined>(undefined);
 
     const [showModalImages, setShowModalImages] = useState<number>(-1);
@@ -27,6 +31,23 @@ const AdvertScreen = ({navigation, route}: BottomTabScreenProps<any>) => {
         }
     }, [route])
 
+    const deleteAdvert = () => {
+        api.delete("adverts/" + animal["id"])
+            .then(() => {
+                showMessage({
+                    type: "success",
+                    message: "Le propriétaire va être contacté pour lui rendre son animal"
+                })
+                navigation.navigate(Routes.HOME_SCREEN);
+            }).catch((e: AxiosError) => {
+            showMessage({
+                type: "danger",
+                message: "Une erreur est survenue"
+            })
+            console.log(e.response)
+        })
+    }
+
     if (!animal) {
         return <></>
     }
@@ -35,8 +56,9 @@ const AdvertScreen = ({navigation, route}: BottomTabScreenProps<any>) => {
         <ScreenContainer withScroll backgroundColor={"#fce9d5"}>
             <Layout style={styles.container}>
                 <View style={styles.headerContainer}>
-                    <TouchableOpacity onPress={() => navigation.goBack()} style={{position: 'absolute', left: 5, padding: 10}}>
-                        <BackIcon />
+                    <TouchableOpacity onPress={() => navigation.goBack()}
+                                      style={{position: 'absolute', left: 5, padding: 10}}>
+                        <BackIcon/>
                     </TouchableOpacity>
                     <Text style={styles.headerTitle}>{animal["animal_name"]}</Text>
                 </View>
@@ -45,8 +67,8 @@ const AdvertScreen = ({navigation, route}: BottomTabScreenProps<any>) => {
                     renderItem={({item, index}) =>
                         <Image
                             key={index}
-                            source={{ uri: API_ADDRESS + item }}
-                            style={{ width:200, height: 200 }}
+                            source={{uri: API_ADDRESS + item}}
+                            style={{width: 200, height: 200}}
                             contentPosition={"center"}
                             onTouchEnd={() => setShowModalImages(index)}
                         />
@@ -64,19 +86,23 @@ const AdvertScreen = ({navigation, route}: BottomTabScreenProps<any>) => {
                 </Modal>
                 <View style={styles.centerContainer}>
                     <View>
-                        <Text style={styles.textRegular}>Sexe : <Text style={{...styles.textRegular, ...styles.textBold}}>{animal["animal_sex"]}</Text></Text>
-                        <Text style={styles.textRegular}>Age : <Text style={{...styles.textRegular, ...styles.textBold}}>{animal["animal_age"]} mois</Text></Text>
+                        <Text style={styles.textRegular}>Sexe : <Text
+                            style={{...styles.textRegular, ...styles.textBold}}>{animal["animal_sex"]}</Text></Text>
+                        <Text style={styles.textRegular}>Age : <Text
+                            style={{...styles.textRegular, ...styles.textBold}}>{animal["animal_age"]} mois</Text></Text>
                     </View>
                     <View>
-                        <Text style={styles.textRegular}>Race : <Text style={{...styles.textRegular, ...styles.textBold}}>{animal["animal_breed"].length > 0 ? animal["animal_breed"] : "Non renseignée"}</Text></Text>
-                        <Text style={styles.textRegular}>Perdu <Text style={{...styles.textRegular, ...styles.textBold}}>le {dayjs(animal["start_date"]).format("DD MMMM YYYY")}</Text></Text>
+                        <Text style={styles.textRegular}>Race : <Text
+                            style={{...styles.textRegular, ...styles.textBold}}>{animal["animal_breed"].length > 0 ? animal["animal_breed"] : "Non renseignée"}</Text></Text>
+                        <Text style={styles.textRegular}>Perdu <Text
+                            style={{...styles.textRegular, ...styles.textBold}}>le {dayjs(animal["start_date"]).format("DD MMMM YYYY")}</Text></Text>
                     </View>
                 </View>
                 <Text style={[styles.textRegular, styles.textBold, styles.info]}>{animal["description"]}</Text>
 
-                <OutlineButton title={"Je l'ai retrouvé(e) !"}  />
+                <OutlineButton title={"Je l'ai retrouvé(e) !"} onPress={deleteAdvert}/>
                 <MapView
-                    style={{ width: "100%", height: 450 }}
+                    style={{width: "100%", height: 450}}
                     initialCamera={{
                         zoom: 15,
                         center: {
